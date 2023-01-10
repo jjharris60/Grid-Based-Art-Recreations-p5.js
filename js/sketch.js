456ecf4241f1c499936d993d1143cce23437efb7
jjharris60 <82051847+jjharris60@users.noreply.github.com>
2023-01-10 10:54:59 +0000

    10:54 (Wave)

--- js/sketch.js
+++ js/sketch.js
@@ -1,5 +1,17 @@
 function setup() {
-    createCanvas(window)
+    createCanvas(windowWidth, windowHeight);
+    let x = 0;
+    let y = 0;
+    let amplitude = windowHeight * 0.1;
+    let frequency = windowWidth * 0.15;
+    let maxAngle = (windowWidth / frequency) * TWO_PI;
+    let theta = 0;
+    while (theta < maxAngle) {
+        y = sin(theta) * amplitude;
+        ellipse(x, y + height * 0.5, 16);
+        theta += 0.2;
+        x = (theta / maxAngle) * windowWidth;
+    }
 }
 function draw() {
 }

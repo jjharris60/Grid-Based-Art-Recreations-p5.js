65b1f987c674110a44443c2f352e6465907db01c
jjharris60 <82051847+jjharris60@users.noreply.github.com>
2023-01-10 11:56:14 +0000

    Multiple waves

--- js/sketch.js
+++ js/sketch.js
@@ -1,17 +1,36 @@
+let x = 0;
+let y = 0;
+let amplitude;
+let maxAngle;
+let frequency;
+let inc = 0.05;
+let offset = 0; // the amount offset from the beginnnijg of the sine wave
+let theta = 0;
 function setup() {
+    amplitude = windowHeight / 3;
+    frequency = windowWidth / 3;
+    maxAngle = (windowWidth / frequency) * TWO_PI
     createCanvas(windowWidth, windowHeight);
-    let x = 0;
-    let y = 0;
-    let amplitude = windowHeight * 0.1;
-    let frequency = windowWidth * 0.15;
-    let maxAngle = (windowWidth / frequency) * TWO_PI;
-    let theta = 0;
-    while (theta < maxAngle) {
-        y = sin(theta) * amplitude;
-        ellipse(x, y + height * 0.5, 16);
-        theta += 0.2;
-        x = (theta / maxAngle) * windowWidth;
-    }
 }
 function draw() {
+    background(0);
+    noStroke();
+    while (theta < maxAngle + offset) {
+        // sine wave 2
+        y = sin(theta - 0.2) * amplitude;
+        fill(60, 5, 90);
+        ellipse(x, y + height * 0.5, 16);
+        // sine wave
+        y = sin(theta) * amplitude;
+        fill(30, 5, 90);
+        ellipse(x, y + height * 0.5, 16);
+        // cosine wave
+        y = cos(theta) * amplitude;
+        fill(80, 10, 90)
+        ellipse(x, y + height * 0.5, 16);
+        theta += 0.2;
+        x = ((theta - offset) / maxAngle) * windowWidth;
+    }
+    offset += inc;
+    theta = offset;
 }

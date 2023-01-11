d952dfe12e2acf95a14b5a02864c4d56dc0dad65
jjharris60 <82051847+jjharris60@users.noreply.github.com>
2023-01-11 22:49:11 +0000

    Creating patterns

---
+++ js/sketch3.js
@@ -0,0 +1,30 @@
+let radius1;
+let angle;
+let step
+function setup() {
+    createCanvas(windowWidth, windowHeight)
+    background(255, 133, 87);
+    noFill()
+    noStroke()
+    r = 250
+    angle = 0
+    step = TWO_PI / 10
+}
+
+
+function draw() {
+    translate(windowWidth / 2, windowHeight / 2);
+    // centre ellipse
+    ellipse(0, 0, 500);
+    stroke(226, 13, 116)
+    strokeWeight(4.5)
+    // initialising variables
+    // converting coordinates
+    let x = r * sin(angle);
+    let y = r * cos(angle);
+    angle = angle + step;
+    ellipse(x, y, 200);
+    stroke(226, 13, 116)
+    ellipse(x, y, 700);
+    stroke(226, 13, 116);
+}

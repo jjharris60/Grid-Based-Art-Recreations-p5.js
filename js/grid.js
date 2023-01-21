66fea9e71692ae16f66065ff0e1825176cb277b0
jjharris60 <82051847+jjharris60@users.noreply.github.com>
2023-01-21 21:21:39 +0000

    createddgrid

--- js/grid.js
+++ js/grid.js
@@ -1,6 +1,7 @@
 function setup() {
-    const CELL_SIZE = 40;
+    noStroke()
     createCanvas(1000, 1000);
+    let CELL_SIZE = 190;
     let x = 0;
     let y = 0;
     let size = CELL_SIZE
@@ -8,15 +9,17 @@ function setup() {
     let currentHeight = size;
     rowCount = 0
     colCount = 0
-
     while (y < width) {
-        while (x > width) {
+        while (x < width) {
             fill(237, 221, 221)
-            circle(x, y, CELL_SIZE)
+            circle(x - 95, y - 95, CELL_SIZE)
+            x += currentWidth;
+            // rowCount++
         }
+        y += currentHeight
+        x = 0
+        // colCount++
     }
 }
-
 function draw() {
-
 }
